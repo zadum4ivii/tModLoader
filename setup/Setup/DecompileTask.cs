6648fea4d3425c3bdf2ee4651fa4a4d514ca36ea
Chicken-Bones <mehvids@gmail.com>
2016-12-10 17:10:23 +1000

    Port setup to newdecompiler ILSpy branch. Decompilation output currently unsatisfactory.

--- setup/Setup/DecompileTask.cs
+++ setup/Setup/DecompileTask.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
 using System.Xml;
-using ICSharpCode.Decompiler;
-using ICSharpCode.Decompiler.Ast;
 using ICSharpCode.ILSpy;
-using ICSharpCode.ILSpy.TextView;
 using Mono.Cecil;
 using Terraria.ModLoader.Properties;
 using static Terraria.ModLoader.Setup.Program;
+using ICSharpCode.Decompiler.CSharp;
+using ICSharpCode.Decompiler.CSharp.OutputVisitor;
+using ICSharpCode.Decompiler.TypeSystem;
+using ICSharpCode.Decompiler.CSharp.Transforms;
 
 namespace Terraria.ModLoader.Setup
 {
@@ -56,7 +57,7 @@ namespace Terraria.ModLoader.Setup
 			}
 		}
 
-		private static readonly CSharpLanguage lang = new CSharpLanguage();
+		private static readonly WholeProjectDecompiler wrappedDecompiler = new WholeProjectDecompiler();
 		private static readonly Guid clientGuid = new Guid("3996D5FA-6E59-4FE4-9F2B-40EEEF9645D5");
 		private static readonly Guid serverGuid = new Guid("85BF1171-A0DC-4696-BFA4-D6E9DC4E0830");
 		public static readonly Version clientVersion = new Version(Settings.Default.ClientVersion);
@@ -92,51 +93,47 @@ namespace Terraria.ModLoader.Setup
 			if (Directory.Exists(FullSrcDir))
 				Directory.Delete(FullSrcDir, true);
 
-			var options = new DecompilationOptions {
-				FullDecompilation = true,
-				CancellationToken = taskInterface.CancellationToken(),
-				SaveAsProjectDirectory = FullSrcDir
-			};
-
 			var items = new List<WorkItem>();
 
 			var serverModule = ReadModule(TerrariaServerPath, serverVersion);
-			var serverSources = GetCodeFiles(serverModule, options).ToList();
-			var serverResources = GetResourceFiles(serverModule, options).ToList();
+			var serverSources = GetCodeFiles(serverModule).ToList();
+			var serverResources = GetResourceFiles(serverModule).ToList();
 
 			var sources = serverSources;
 			var resources = serverResources;
 			var infoModule = serverModule;
 			if (!serverOnly) {
 				var clientModule = !serverOnly ? ReadModule(TerrariaPath, clientVersion) : null;
-				var clientSources = GetCodeFiles(clientModule, options).ToList();
-				var clientResources = GetResourceFiles(clientModule, options).ToList();
+				var clientSources = GetCodeFiles(clientModule).ToList();
+				var clientResources = GetResourceFiles(clientModule).ToList();
 
 				sources = CombineFiles(clientSources, sources, src => src.Key);
 				resources = CombineFiles(clientResources, resources, res => res.Item1);
 				infoModule = clientModule;
 
-				items.Add(new WorkItem("Writing Terraria" + lang.ProjectFileExtension,
-					() => WriteProjectFile(clientModule, clientGuid, clientSources, clientResources, options)));
+				items.Add(new WorkItem("Writing Terraria.csproj",
+					() => WriteProjectFile(clientModule, clientGuid, clientSources, clientResources)));
 
-				items.Add(new WorkItem("Writing Terraria" + lang.ProjectFileExtension + ".user",
-					() => WriteProjectUserFile(clientModule, SteamDir, options)));
+				items.Add(new WorkItem("Writing Terraria.csproj.user",
+					() => WriteProjectUserFile(clientModule, SteamDir)));
 			}
 
-			items.Add(new WorkItem("Writing TerrariaServer"+lang.ProjectFileExtension,
-				() => WriteProjectFile(serverModule, serverGuid, serverSources, serverResources, options)));
+			items.Add(new WorkItem("Writing TerrariaServer.csproj",
+				() => WriteProjectFile(serverModule, serverGuid, serverSources, serverResources)));
 
-			items.Add(new WorkItem("Writing TerrariaServer"+lang.ProjectFileExtension+".user",
-				() => WriteProjectUserFile(serverModule, SteamDir, options)));
-			
+			items.Add(new WorkItem("Writing TerrariaServer.csproj.user",
+				() => WriteProjectUserFile(serverModule, SteamDir)));
+
+			DecompilerTypeSystem ts = new DecompilerTypeSystem(infoModule);
+
 			items.Add(new WorkItem("Writing Assembly Info",
-				() => WriteAssemblyInfo(infoModule, options)));
-			
+				() => WriteAssemblyInfo(infoModule, ts)));
+
 			items.AddRange(sources.Select(src => new WorkItem(
-				"Decompiling: "+src.Key, () => DecompileSourceFile(src, options))));
+				"Decompiling: "+src.Key, () => DecompileSourceFile(src, ts))));
 
 			items.AddRange(resources.Select(res => new WorkItem(
-				"Extracting: " + res.Item1, () => ExtractResource(res, options))));
+				"Extracting: " + res.Item1, () => ExtractResource(res))));
 			
 			ExecuteParallel(items, maxDegree: Settings.Default.SingleDecompileThread ? 1 : 0);
 		}
@@ -155,38 +152,40 @@ namespace Terraria.ModLoader.Setup
 		}
 
 #region ReflectedMethods
-		private static readonly MethodInfo _IncludeTypeWhenDecompilingProject = typeof(CSharpLanguage)
+		private static readonly MethodInfo _IncludeTypeWhenDecompilingProject = typeof(WholeProjectDecompiler)
 			.GetMethod("IncludeTypeWhenDecompilingProject", BindingFlags.NonPublic | BindingFlags.Instance);
 
-		public static bool IncludeTypeWhenDecompilingProject(TypeDefinition type, DecompilationOptions options) {
-			return (bool)_IncludeTypeWhenDecompilingProject.Invoke(lang, new object[] { type, options });
+		public static bool IncludeTypeWhenDecompilingProject(TypeDefinition type) {
+			return (bool)_IncludeTypeWhenDecompilingProject.Invoke(wrappedDecompiler, new object[] { type });
 		}
 
-		private static readonly MethodInfo _WriteProjectFile = typeof(CSharpLanguage)
+		private static readonly MethodInfo _CreateDecompiler = typeof(WholeProjectDecompiler)
+			.GetMethod("CreateDecompiler", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		public CSharpDecompiler CreateDecompiler(DecompilerTypeSystem ts) {
+			var decompiler = (CSharpDecompiler)_CreateDecompiler.Invoke(wrappedDecompiler, new object[] { ts });
+			decompiler.CancellationToken = taskInterface.CancellationToken();
+			return decompiler;
+		}
+
+		private static readonly MethodInfo _WriteProjectFile = typeof(WholeProjectDecompiler)
 			.GetMethod("WriteProjectFile", BindingFlags.NonPublic | BindingFlags.Instance);
 
 		public static void WriteProjectFile(TextWriter writer, IEnumerable<Tuple<string, string>> files, ModuleDefinition module) {
-			_WriteProjectFile.Invoke(lang, new object[] { writer, files, module });
-		}
-
-		private static readonly MethodInfo _CleanUpName = typeof(DecompilerTextView)
-			.GetMethod("CleanUpName", BindingFlags.NonPublic | BindingFlags.Static);
-
-		public static string CleanUpName(string name) {
-			return (string)_CleanUpName.Invoke(null, new object[] { name });
+			_WriteProjectFile.Invoke(wrappedDecompiler, new object[] { writer, files, module });
 		}
 #endregion
 
 		//from ICSharpCode.ILSpy.CSharpLanguage
-		private static IEnumerable<IGrouping<string, TypeDefinition>> GetCodeFiles(ModuleDefinition module, DecompilationOptions options) {
-			return module.Types.Where(t => IncludeTypeWhenDecompilingProject(t, options))
+		private static IEnumerable<IGrouping<string, TypeDefinition>> GetCodeFiles(ModuleDefinition module) {
+			return module.Types.Where(t => IncludeTypeWhenDecompilingProject(t))
 				.GroupBy(type => {
-					var file = CleanUpName(type.Name) + lang.FileExtension;
-					return string.IsNullOrEmpty(type.Namespace) ? file : Path.Combine(CleanUpName(type.Namespace), file);
+					var file = WholeProjectDecompiler.CleanUpFileName(type.Name) + ".cs";
+					return string.IsNullOrEmpty(type.Namespace) ? file : Path.Combine(WholeProjectDecompiler.CleanUpFileName(type.Namespace), file);
 				}, StringComparer.OrdinalIgnoreCase);
 		}
 
-		private static IEnumerable<Tuple<string, EmbeddedResource>> GetResourceFiles(ModuleDefinition module, DecompilationOptions options) {
+		private static IEnumerable<Tuple<string, EmbeddedResource>> GetResourceFiles(ModuleDefinition module) {
 			return module.Resources.OfType<EmbeddedResource>().Select(res => {
 				var path = res.Name;
 				path = path.Replace("Terraria.Libraries.", "Terraria.Libraries\\");
@@ -207,8 +206,8 @@ namespace Terraria.ModLoader.Setup
 			return list;
 		}
 
-		private static void ExtractResource(Tuple<string, EmbeddedResource> res, DecompilationOptions options) {
-			var path = Path.Combine(options.SaveAsProjectDirectory, res.Item1);
+		private void ExtractResource(Tuple<string, EmbeddedResource> res) {
+			var path = Path.Combine(FullSrcDir, res.Item1);
 			CreateParentDirectory(path);
 
 			var s = res.Item2.GetResourceStream();
@@ -217,49 +216,37 @@ namespace Terraria.ModLoader.Setup
 				s.CopyTo(fs);
 		}
 
-		private static void DecompileSourceFile(IGrouping<string, TypeDefinition> src, DecompilationOptions options) {
-			var path = Path.Combine(options.SaveAsProjectDirectory, src.Key);
+		private void DecompileSourceFile(IGrouping<string, TypeDefinition> src, DecompilerTypeSystem ts) {
+			var path = Path.Combine(FullSrcDir, src.Key);
 			CreateParentDirectory(path);
 
 			using (var w = new StreamWriter(path)) {
-				var builder = new AstBuilder(
-					new DecompilerContext(src.First().Module) {
-						CancellationToken = options.CancellationToken,
-						Settings = options.DecompilerSettings
-					});
-
-				foreach (var type in src)
-					builder.AddType(type);
-
-				builder.GenerateCode(new PlainTextOutput(w));
+				var decompiler = CreateDecompiler(ts);
+				var syntaxTree = decompiler.DecompileTypes(src.ToArray());
+				syntaxTree.AcceptVisitor(new CSharpOutputVisitor(w, wrappedDecompiler.Settings.CSharpFormattingOptions));
 			}
 		}
 
-		private static void WriteAssemblyInfo(ModuleDefinition module, DecompilationOptions options) {
-			var path = Path.Combine(options.SaveAsProjectDirectory, Path.Combine("Properties", "AssemblyInfo" + lang.FileExtension));
+		private void WriteAssemblyInfo(ModuleDefinition module, DecompilerTypeSystem ts) {
+			var path = Path.Combine(FullSrcDir, Path.Combine("Properties", "AssemblyInfo.cs"));
 			CreateParentDirectory(path);
 
 			using (var w = new StreamWriter(path)) {
-				var builder = new AstBuilder(
-					new DecompilerContext(module) {
-						CancellationToken = options.CancellationToken,
-						Settings = options.DecompilerSettings
-					});
-
-				builder.AddAssembly(module, true);
-				builder.GenerateCode(new PlainTextOutput(w));
+				var decompiler = CreateDecompiler(ts);
+				decompiler.AstTransforms.Add(new RemoveCompilerGeneratedAssemblyAttributes());
+				var syntaxTree = decompiler.DecompileModuleAndAssemblyAttributes();
+				syntaxTree.AcceptVisitor(new CSharpOutputVisitor(w, wrappedDecompiler.Settings.CSharpFormattingOptions));
 			}
 		}
 
-		private static void WriteProjectFile(ModuleDefinition module, Guid guid,
+		private void WriteProjectFile(ModuleDefinition module, Guid guid,
 				IEnumerable<IGrouping<string, TypeDefinition>> sources, 
-				IEnumerable<Tuple<string, EmbeddedResource>> resources,
-				DecompilationOptions options) {
+				IEnumerable<Tuple<string, EmbeddedResource>> resources) {
 
 			//flatten the file list
 			var files = sources.Select(src => Tuple.Create("Compile", src.Key))
 				.Concat(resources.Select(res => Tuple.Create("EmbeddedResource", res.Item1)))
-				.Concat(new[] { Tuple.Create("Compile", Path.Combine("Properties", "AssemblyInfo" + lang.FileExtension)) });
+				.Concat(new[] { Tuple.Create("Compile", Path.Combine("Properties", "AssemblyInfo.cs")) });
 
 			//fix the guid and add a value to the CommandLineArguments field so the method doesn't crash
 			var claField = typeof(App).GetField("CommandLineArguments", BindingFlags.Static | BindingFlags.NonPublic);
@@ -270,8 +257,8 @@ namespace Terraria.ModLoader.Setup
 			guidField.SetValue(claInst, guid);
 			claField.SetValue(null, claInst);
 
-			var path = Path.Combine(options.SaveAsProjectDirectory,
-				Path.GetFileNameWithoutExtension(module.Name) + lang.ProjectFileExtension);
+			var path = Path.Combine(FullSrcDir,
+				Path.GetFileNameWithoutExtension(module.Name) + ".csproj");
 			CreateParentDirectory(path);
 
 			using (var w = new StreamWriter(path))
@@ -280,9 +267,9 @@ namespace Terraria.ModLoader.Setup
 				w.Write(Environment.NewLine);
 		}
 
-		private static void WriteProjectUserFile(ModuleDefinition module, string debugWorkingDir, DecompilationOptions options) {
-			var path = Path.Combine(options.SaveAsProjectDirectory,
-				Path.GetFileNameWithoutExtension(module.Name) + lang.ProjectFileExtension + ".user");
+		private void WriteProjectUserFile(ModuleDefinition module, string debugWorkingDir) {
+			var path = Path.Combine(FullSrcDir,
+				Path.GetFileNameWithoutExtension(module.Name) + ".csproj.user");
 			CreateParentDirectory(path);
 
 			using (var w = new StreamWriter(path))
